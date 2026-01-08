1fa5eee3cee3811b69c4cfb7a7161658fa95f22d
pythonh1234-cyber <pythonh1234@gmail.com>
2026-01-08 20:17:23 +0530

    Remove old Odra files, keep only simple contract

--- contracts/src/bin/escrow.rs
+++
@@ -1,5 +0,0 @@
-use civictrust_contracts::escrow::CivicTrustEscrow;
-
-fn main() {
-    odra_casper_wasm_env::run(CivicTrustEscrow::default());
-}
--- contracts/src/bin/reputation.rs
+++
@@ -1,5 +0,0 @@
-use civictrust_contracts::reputation::ReputationNFT;
-
-fn main() {
-    odra_casper_wasm_env::run(ReputationNFT::default());
-}
